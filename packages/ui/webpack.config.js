3d88965a9a4bed167fe929f900c1a86cee7fa876
FocusCookie <s.lueddemann@mailbox.org>
2023-09-14 10:58:18 +0200

    :wrench: Ui webpack config

---
+++ packages/ui/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    library: "formCuiontrols",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "dist"),
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    "styled-components": "styled-components",
+  },
+  mode: "production",
+};
