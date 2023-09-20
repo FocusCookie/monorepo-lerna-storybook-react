b4212c756d63c5d1ccd42a8440060963170f8c1b
FocusCookie <s.lueddemann@mailbox.org>
2023-09-20 09:21:26 +0200

    :white_check_mark: refactor: Ui Button Stories

--- packages/ui/src/Button/Button.stories.tsx
+++ packages/ui/src/Button/Button.stories.tsx
@@ -16,30 +16,102 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = {};
 
-export const Variants: ComponentStory<typeof Button> = (args) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "1rem",
-      width: "fit-content",
-    }}
-  >
-    <Button {...args} variant="primary" />
-    <Button {...args} variant="secondary" />
-  </div>
-);
+export const VariantPrimary = Template.bind({});
+VariantPrimary.args = {
+  variant: "primary",
+};
 
-export const Sizes: ComponentStory<typeof Button> = (args) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "1rem",
-      width: "fit-content",
-    }}
-  >
-    <Button {...args} size="base" />
-    <Button {...args} size="large" />
-  </div>
-);
+export const VariantSecondary = Template.bind({});
+VariantSecondary.args = {
+  variant: "secondary",
+};
+
+export const SizeLarge = Template.bind({});
+SizeLarge.args = {
+  size: "large",
+};
+
+export const SizeBase = Template.bind({});
+SizeBase.args = {
+  size: "base",
+};
+
+export const PrimaryLarge = Template.bind({});
+PrimaryLarge.args = {
+  variant: "primary",
+  size: "large",
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  variant: "primary",
+  size: "base",
+};
+
+export const SecondaryLarge = Template.bind({});
+SecondaryLarge.args = {
+  variant: "secondary",
+  size: "large",
+};
+
+export const SecondarySmall = Template.bind({});
+SecondarySmall.args = {
+  variant: "secondary",
+  size: "base",
+};
+
+export const PrimaryDisabled = Template.bind({});
+PrimaryDisabled.args = {
+  variant: "primary",
+  disabled: true,
+};
+
+export const SecondaryDisabled = Template.bind({});
+SecondaryDisabled.args = {
+  variant: "secondary",
+  disabled: true,
+};
+
+export const PrimaryLargeDisabled = Template.bind({});
+PrimaryLargeDisabled.args = {
+  variant: "primary",
+  size: "large",
+  disabled: true,
+};
+
+export const SecondaryLargeDisabled = Template.bind({});
+SecondaryLargeDisabled.args = {
+  variant: "secondary",
+  size: "large",
+  disabled: true,
+};
+
+export const PrimaryDisabledLoading = Template.bind({});
+PrimaryDisabledLoading.args = {
+  variant: "primary",
+  disabled: true,
+  loading: true,
+};
+
+export const SecondaryDisabledLoading = Template.bind({});
+SecondaryDisabledLoading.args = {
+  variant: "secondary",
+  disabled: true,
+  loading: true,
+};
+
+export const PrimaryDisabledLoadingLarge = Template.bind({});
+PrimaryDisabledLoadingLarge.args = {
+  variant: "primary",
+  size: "large",
+  disabled: true,
+  loading: true,
+};
+
+export const SecondaryDisabledLoadingLarge = Template.bind({});
+SecondaryDisabledLoadingLarge.args = {
+  variant: "secondary",
+  size: "large",
+  disabled: true,
+  loading: true,
+};
